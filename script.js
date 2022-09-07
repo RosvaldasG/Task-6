dcd3dd9d0bfabf3c223458d699f60f2d3d317822
RosvaldasG <rosvaldasggg@gmail.com>
2022-09-07 13:03:16 +0300

    Task-6

---
+++ script.js
@@ -0,0 +1,32 @@
+/* ------------------------------ TASK 6 -----------------------------------
+Turimas "users" masyvas. 
+
+Parašykite funckijas, kurios atlikas nurodytas užduotis:
+1. funkcija "getUserAverageAge" - kaip argumentą priims masyvą ir duoto masyvo 
+atveju grąžins visų "users" amžiaus visurkį kaip skaičių.
+2. funkcija "getUsersNames" -  kaip argumentą priims masyvą ir duoto masyvo 
+atveju grąžins visų "users" vardus naujame masyve pvz., ['John Smith', 'Ann Smith'..].
+-------------------------------------------------------------------------- */
+
+const users = [
+  { id: "1", name: "John Smith", age: 20 },
+  { id: "2", name: "Ann Smith", age: 24 },
+  { id: "3", name: "Tom Jones", age: 31 },
+  { id: "4", name: "Rose Peterson", age: 17 },
+  { id: "5", name: "Alex John", age: 25 },
+  { id: "6", name: "Ronald Jones", age: 63 },
+  { id: "7", name: "Elton Smith", age: 16 },
+  { id: "8", name: "Simon Peterson", age: 30 },
+  { id: "9", name: "Daniel Cane", age: 51 },
+];
+
+const getUserAverageAge = (arr) => {
+  const sum = arr.reduce((oneValue, secValue) => oneValue + secValue);
+  return sum / arr.length;
+};
+
+const averAge = users.map((person) => person.age);
+console.log("Amžiaus vidurkis: " + getUserAverageAge(averAge));
+
+const getUsersNames = users.map((data) => data.name);
+console.log(getUsersNames);
